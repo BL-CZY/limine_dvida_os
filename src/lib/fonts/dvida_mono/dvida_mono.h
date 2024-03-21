dbe83363c316f17715ede1f489eba03e86121a9a
BL_CZY <jiguangllsw@gmail.com>
2024-03-21 21:52:34 +0100

    dvida_mono

---
+++ src/lib/fonts/dvida_mono/dvida_mono.h
@@ -0,0 +1,8 @@
+#ifndef DVIDA_MONO_H
+#define DVIDA_MONO_H
+
+#include "../fonts.h"
+
+extern font_char_t dvida_mono[128];
+
+#endif
---
+++ src/lib/fonts/fonts.h
@@ -0,0 +1,7 @@
+#ifndef FONTS_H
+#define FONTS_H
+
+struct font_char;
+typedef font_char font_char_t;
+
+#endif
